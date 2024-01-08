7276f2e740a5fc3c42103d8adec210f9dc4d6a97
chameleon05119 <chameleon05119@gmail.com>
2024-01-08 18:32:29 +0900

    会員登録後に会員詳細情報ページに遷移するように修正

--- src/views/member/MemberAdd.vue
+++ src/views/member/MemberAdd.vue
@@ -16,7 +16,12 @@ const member: Member = reactive({
 const onAdd = (): void => {
   console.log(member);
   memberList.set(member.id, member);
-  router.push({ name: "MemberList" });
+  router.push({
+    name: "MemberDetail",
+    params: {
+      id: member.id,
+    },
+  });
 };
 </script>
 <template>
