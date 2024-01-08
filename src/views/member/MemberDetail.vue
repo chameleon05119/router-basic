9e3f96191577045920c64eac8df3d8c79e10b3d9
chameleon05119 <chameleon05119@gmail.com>
2024-01-08 11:38:51 +0900

    会員詳細ページの作成

---
+++ src/views/member/MemberDetail.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+<template>
+  <h1>会員管理</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li>
+        <RouterLink :to="{ name: 'AppTop' }"> TOP</RouterLink>
+      </li>
+      <li>
+        <RouterLink :to="{ name: 'MemberList' }"> 会員リスト</RouterLink>
+      </li>
+      <li>会員詳細情報</li>
+    </ul>
+  </nav>
+  <section>
+    <h2>会員詳細情報</h2>
+  </section>
+</template>
