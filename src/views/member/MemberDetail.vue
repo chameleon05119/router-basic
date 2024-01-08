bdaf3c14af3840025c59df5b24f5b8ad5011c007
chameleon05119 <chameleon05119@gmail.com>
2024-01-08 17:39:37 +0900

    会員詳細情報ページで、会員IDをもとに会員情報を表示する

--- src/views/member/MemberDetail.vue
+++ src/views/member/MemberDetail.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
+import { inject, computed } from "vue";
 import { RouterLink } from "vue-router";
+import type { Member } from "@/interfaces";
+
+interface Props {
+  id: number;
+}
+const props = defineProps<Props>();
+const memberList = inject("memberList") as Map<number, Member>;
+const member = computed((): Member => {
+  return memberList.get(props.id) as Member;
+});
+const localNote = computed((): string => {
+  let localNote = "--";
+  if (member.value.note != undefined) {
+    localNote = member.value.note;
+  }
+  return localNote;
+});
 </script>
 <template>
   <h1>会員管理</h1>
@@ -16,5 +34,17 @@ import { RouterLink } from "vue-router";
   </nav>
   <section>
     <h2>会員詳細情報</h2>
+    <dl>
+      <dt>ID</dt>
+      <dd>{{ member.id }}</dd>
+      <dt>名前</dt>
+      <dd>{{ member.name }}</dd>
+      <dt>メールアドレス</dt>
+      <dd>{{ member.email }}</dd>
+      <dt>保有ポイント</dt>
+      <dd>{{ member.points }}</dd>
+      <dt>備考</dt>
+      <dd>{{ localNote }}</dd>
+    </dl>
   </section>
 </template>
