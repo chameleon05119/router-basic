d67e63b90c612937bc8e6f935e42c8a5b319ee2e
chameleon05119 <chameleon05119@gmail.com>
2024-01-09 20:16:24 +0900

    Not Foundページの追加

---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+router.push("/not-found");
+</script>
+<template>
+  <h1>404 NotFound</h1>
+</template>
